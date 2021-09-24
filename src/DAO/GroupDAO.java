b685133cf20b3e51a323ace9490a05909b09114a
catng19 <nguyenconganhtuan19@gmail.com>
2021-09-24 23:56:51 +0700

    update groupDAO

--- src/DAO/GroupDAO.java
+++ src/DAO/GroupDAO.java
@@ -8,10 +8,9 @@ package DAO;
 import static DAO.DAO.con;
 import Model.Group;
 import Model.User;
+import com.mysql.cj.protocol.Resultset;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 /**
  *
@@ -20,24 +19,37 @@ import java.time.LocalTime;
 public class GroupDAO extends DAO {
 
     public boolean addUser(User user, Group group, String note) {
-        String sql = "INSERT INTO tbljoingroup(note, user_id, group_id) VALUES(?, ?, ?)";
-        boolean res = true;
+        String sql = "SELECT * FROM tblgroup WHERE group_id=?";
         try {
             PreparedStatement ps = con.prepareStatement(sql);
-            ps.setString(1, note);
-            ps.setInt(2, user.getUser_id());
-            ps.setInt(3, group.getGroup_id());
-            ps.executeUpdate();
+            ps.setInt(1, group.getGroup_id());
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.equals(null)) {
+                return false;
+            } else {
+                sql = "INSERT INTO tbljoingroup(note, user_id, group_id) VALUES(?, ?, ?)";
+                ps = con.prepareStatement(sql);
+                ps.setString(1, note);
+                ps.setInt(2, user.getUser_id());
+                ps.setInt(3, group.getGroup_id());
+                ps.executeUpdate();
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            res = false;
+            return false;
         }
-        return res;
+        return true;
     }
-
+    
+//    Overload
+    public boolean addUser(User user, Group group){
+        return addUser(user, group, "");
+    }
+            
+            
     public boolean delUser(User user, Group group) {
-        String sql = "DELETE FROM tbljoininggroup WHERE user_id=? AND group_id=?";
-        boolean res = true;
+        String sql = "DELETE FROM tbljoingroup WHERE user_id=? AND group_id=?";
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, user.getUser_id());
@@ -46,25 +58,39 @@ public class GroupDAO extends DAO {
 
         } catch (Exception e) {
             e.printStackTrace();
-            res = false;
+            return false;
         }
-        return res;
+        return true;
     }
 
     public boolean delGroup(Group group) {
-        String sql = "DELETE FROM tbljoininggroup WHERE group_id=?";
-        boolean res = true;
+        String sql = "DELETE FROM tbljoingroup WHERE group_id=?";
         try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, group.getGroup_id());
             ps.executeUpdate();
+
+            sql = "DELETE FROM tblgroup WHERE group_id=?";
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, group.getGroup_id());
+            ps.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
-            res = false;
+            return false;
         }
-        
-        return res;
+
+        return true;
     }
 
-//    public boolean createGroup()
+    public boolean createGroup(User user, Group group) {
+        String sql = "INSERT INTO tblgroup(group_name, group_type) VALUES(?, ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, group.getGroup_name());
+            ps.set(2, group.getGroup_type());
+            ResultSet rs = ps.executeQuery();
+        } catch (Exception e) {
+        }
+                
+    }
 }
